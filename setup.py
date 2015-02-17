f376bdc91a8b12785e60e7f0072f2f732e8ca49a
Matthijs Otterloo <matthijsotterloo@users.noreply.github.com>
2015-02-17 13:22:09 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
-
 from setuptools import setup
-
 setup(name='SiriAPI8',
-      version='8.0.1', #TODO: Change version on new release
+      version='8.0.1',
       description='SiriAPI8 - use Siri to interact with Python scripts',
       author='Robin Meis',
-      author_email='hc.devel@yahoo.de', #NO PERSONAL SUPPORT!!!
+      author_email='hc.devel@yahoo.de',
       url='https://github.com/HcDevel/SiriAPI8',
       packages=['SiriAPI8'],
       platforms = ('Any'))
